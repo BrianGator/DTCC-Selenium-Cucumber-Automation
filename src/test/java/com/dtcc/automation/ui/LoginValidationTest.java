9e9aae5d46a8131c6457d02777ee38cfac19ffb2
Brian McCarthy <briansmc@gmail.com>
2026-06-03 16:43:46 -0400

    Make login validation stable for CI

--- src/test/java/com/dtcc/automation/ui/LoginValidationTest.java
+++ src/test/java/com/dtcc/automation/ui/LoginValidationTest.java
@@ -2,7 +2,6 @@ package com.dtcc.automation.ui;
 
 import com.dtcc.automation.base.UiTestBase;
 import com.dtcc.automation.pages.LoginPage;
-import com.dtcc.automation.pages.OrderLifecyclePage;
 import com.dtcc.automation.utils.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -10,13 +9,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class LoginValidationTest extends UiTestBase {
-    @Test(description = "Valid login redirects user to catalogue/order lifecycle area")
+    @Test(description = "Valid login displays the catalogue area in the local mock app")
     public void validUserCanLoginToMockOrderApplication() {
         WebDriver driver = openMockOrderApplication();
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("demo_user", "demo_password");
-        OrderLifecyclePage orderPage = new OrderLifecyclePage(driver);
-        Assert.assertTrue(orderPage.confirmationText().isEmpty() || driver.getPageSource().contains("Catalogue"));
+        Assert.assertTrue(driver.getPageSource().contains("Catalogue"), "Catalogue should be available after login.");
     }
 
     @AfterMethod(alwaysRun = true)
