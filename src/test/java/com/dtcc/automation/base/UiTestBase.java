e19b9f4c32d3350bf9cb3eccaa0f9ac0e4b9c81f
Brian McCarthy <briansmc@gmail.com>
2026-06-03 17:48:48 -0400

    Add shared UI test driver lifecycle

--- src/test/java/com/dtcc/automation/base/UiTestBase.java
+++ src/test/java/com/dtcc/automation/base/UiTestBase.java
@@ -3,14 +3,34 @@ package com.dtcc.automation.base;
 import com.dtcc.automation.constants.FrameworkConstants;
 import com.dtcc.automation.utils.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.nio.file.Path;
 
 public class UiTestBase {
+    protected WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUpUiDriver() {
+        driver = WebDriverFactory.createDriver();
+    }
+
     protected WebDriver openMockOrderApplication() {
-        WebDriver driver = WebDriverFactory.createDriver();
-        String fileUrl = Path.of(FrameworkConstants.MOCK_APP_PATH).toAbsolutePath().toUri().toString();
-        driver.get(fileUrl);
+        if (driver == null) {
+            driver = WebDriverFactory.createDriver();
+        }
+        driver.get(mockAppUrl());
         return driver;
+    }
+
+    protected String mockAppUrl() {
+        return Path.of(FrameworkConstants.MOCK_APP_PATH).toAbsolutePath().toUri().toString();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDownUiDriver() {
+        WebDriverFactory.quitDriver();
+        driver = null;
     }
 }
